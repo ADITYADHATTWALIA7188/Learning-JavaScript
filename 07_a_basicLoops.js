fa27d507d83287d486a6a650cc7fc93a71fb1b25
Aditya Dhattwalia <aditya2006dhattwalia@gmail.com>
2026-07-29 13:17:21 +0530

    Learning basic loop in file 07_a_basicLoops.js

---
+++ 07_a_basicLoops.js
@@ -0,0 +1,17 @@
+//for loop
+for( let i = 0; i<10 ; i++){
+    console.log(i);
+}
+
+//while loop
+let i = 10;
+while(i>=0){
+    console.log(i);
+    i--
+}
+//do while loop - in this there must be one printing by default
+let i = 5;
+do{
+    console.log(i);
+    i++
+}while(i<=10);
